2b40d4511a41a29bc85fdbb943015d007b86f56e
Alessandro Grossano <grossano.alessandro@gmail.com>
2020-08-07 15:20:06 +0200

    feat: update state properties

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    //l'unica volta in cui si assegna direttamene una proprietà a this.state (vedi sotto 'setState)
+    this.state = { lat: null };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        //chiamo setState per aggiornare lo stato
+        this.setState({ lat: position.coords.latitude });
+      },
       (err) => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
